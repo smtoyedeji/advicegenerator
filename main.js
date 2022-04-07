007f96bab68251dbd2fb9118149029193018dbd0
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-04-07 16:41:24 +0100

    Advice generator app completed

---
+++ main.js
@@ -0,0 +1,16 @@
+//get variables from html elements
+const advice = document.getElementById("advice");
+const heading = document.getElementById("heading--el")
+
+
+//run a setInterval function that fetches data every 5secs
+window.setInterval(function() {
+    //fetch data from advicesslip
+    fetch("https://api.adviceslip.com/advice")
+    .then(res => res.json())
+    .then(data => {
+        //inject data into html elements
+        advice.textContent = `"${data.slip.advice}"`
+        heading.textContent = `Advice #${data.slip.id}`
+    })
+}, 5000)
