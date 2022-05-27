81b72471dc1acb489620220e20b133b3d422b146
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-05-27 17:00:31 +0100

    adviceGenerator first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # advicegenerator.github.io
+
+.container > img:hover {
+    filter: brightness(50%);
+}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //get variables from html elements
 const advice = document.getElementById("advice");
 const heading = document.getElementById("heading--el")
-
+console.log(heading)
 
 //run a setInterval function that fetches data every 5secs
 window.setInterval(function() {
@@ -9,6 +9,7 @@ window.setInterval(function() {
     fetch("https://api.adviceslip.com/advice")
     .then(res => res.json())
     .then(data => {
+        console.log(data)
         //inject data into html elements
         advice.textContent = `"${data.slip.advice}"`
         heading.textContent = `Advice #${data.slip.id}`
